776b46832aae640ccabd5a9d0f34261eadabce2c
joonho0410 <76806109+joonho0410@users.noreply.github.com>
2025-03-24 17:57:02 +0900

    Create 9989_Count_Element_Number_To_Object.ts

---
+++ 9989_Count_Element_Number_To_Object.ts
@@ -0,0 +1,103 @@
+/*
+  9989 - Count Element Number To Object
+  -------
+  by 凤之兮原 (@kongmingLatern) #medium
+
+  ### Question
+
+  With type ``CountElementNumberToObject``, get the number of occurrences of every item from an array and return them in an object. For example:
+
+  ~~~ts
+  type Simple1 = CountElementNumberToObject<[]> // return {}
+  type Simple2 = CountElementNumberToObject<[1,2,3,4,5]>
+  // return {
+  //   1: 1,
+  //   2: 1,
+  //   3: 1,
+  //   4: 1,
+  //   5: 1
+  // }
+
+  type Simple3 = CountElementNumberToObject<[1,2,3,4,5,[1,2,3]]>
+  // return {
+  //   1: 2,
+  //   2: 2,
+  //   3: 2,
+  //   4: 1,
+  //   5: 1
+  // }
+  ~~~
+
+  > View on GitHub: https://tsch.js.org/9989
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type Array2Flat<T extends any[], Ret extends any []= []> = T extends [infer First, ...infer Rest]
+  ? First extends any[] 
+    ? [...Array2Flat<First>, ...Array2Flat<Rest, Ret>]
+    : Array2Flat<Rest, [...Ret, First]>
+  : Ret
+
+type Count<A extends any[], Key, Ret extends any[] = []> = A extends [infer First, ...infer Rest]
+  ? Equal<First, Key> extends true
+    ? Count<Rest, Key, [...Ret, 1]>
+    : Count<Rest, Key, Ret>
+  : Ret['length']
+
+type CountElementNumberToObject<T extends any[], A extends any []= Array2Flat<T>> = {
+  [ Key in A[number] as Key]: Count<A, Key>
+}
+
+
+
+
+type TestFlat = Array2Flat<[1, 2, 3, 4, 5, [1, 2, 3]]>
+type TestCount = Count<[1,1,2,3], 1> 
+type Test = CountElementNumberToObject<[1, 2, 3, 4, 5]>
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<CountElementNumberToObject<[1, 2, 3, 4, 5]>, {
+    1: 1
+    2: 1
+    3: 1
+    4: 1
+    5: 1
+  } >>,
+  Expect<Equal<CountElementNumberToObject<[1, 2, 3, 4, 5, [1, 2, 3]]>, {
+    1: 2
+    2: 2
+    3: 2
+    4: 1
+    5: 1
+  }>>,
+  Expect<Equal<CountElementNumberToObject<[1, 2, 3, 4, 5, [1, 2, 3, [4, 4, 1, 2]]]>, {
+    1: 3
+    2: 3
+    3: 2
+    4: 3
+    5: 1
+  }>>,
+  Expect<Equal<CountElementNumberToObject<[never]>, {}>>,
+  Expect<Equal<CountElementNumberToObject<['1', '2', '0']>, {
+    0: 1
+    1: 1
+    2: 1
+  }>>,
+  Expect<Equal<CountElementNumberToObject<['a', 'b', ['c', ['d']]]>, {
+    'a': 1
+    'b': 1
+    'c': 1
+    'd': 1
+  }>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/9989/answer
+  > View solutions: https://tsch.js.org/9989/solutions
+  > More Challenges: https://tsch.js.org
+*/
